f3dc0540153856766ab1dabdd1cd89d08dd09507
Nathan Shearer <shearern@gmail.com>
2016-11-19 01:22:24 -0800

    Flushed out relative path

--- src/test_RelativePath.py
+++ src/test_RelativePath.py
@@ -12,7 +12,10 @@ class TestRelativePath(TestCase):
         child = root.join('sub_file.txt')
 
         self.assertEquals(str(root), os.path.normpath('.'))
-        self.assertEquals(root.abs, os.path.normpath('/root/path'))
+        self.assertEquals(os.path.normpath(root.full_path),
+                          os.path.normpath('/root/path'))
 
-        self.assertEquals(str(child), os.path.normpath('sub_file.txt'))
-        self.assertEquals(child.abs, os.path.normpath('/root/path/sub_file.txt'))
+        self.assertEquals(os.path.normpath(str(child)),
+                          os.path.normpath('sub_file.txt'))
+        self.assertEquals(os.path.normpath(child.full_path),
+                          os.path.normpath('/root/path/sub_file.txt'))
