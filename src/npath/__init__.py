f34efd5bdd2e7a0726ca933df503501c53812566
Nathan Shearer <shearern@gmail.com>
2016-11-22 17:15:40 -0800

    bug fix: Preserving relative_to in File + Directory factory

--- src/npath/__init__.py
+++ src/npath/__init__.py
@@ -5,8 +5,13 @@ from File import File
 from Directory import Directory
 from InvalidFileObject import InvalidFileObject
 
-def file_object_factory(*path_parts):
-    path = Path(*path_parts)
+def file_object_factory(creator, *path_parts):
+
+    if len(path_parts) == 1 and path_parts[0].__class__ is Path:
+        path = path_parts[0]
+    else:
+        path = Path(*path_parts)
+
     if path.is_file:
         return File(path)
     elif path.is_dir:
