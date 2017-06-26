2339c207cc64ebd6bc5562ec410128a88e794094
Nathan Shearer <nshearer@ewu.edu>
2017-06-26 11:36:32 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='npath',
+    version='1.1.0',
+    packages=['npath'],
+    package_dir={'': 'src'},
+    url='https://github.com/shearern/npath',
+    license='GPL-3.0',
+    author='Nathan Shearer',
+    author_email='shearern@gmail.com',
+    description='Work with os.path as Path objects.'
+)
